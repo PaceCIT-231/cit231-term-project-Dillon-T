450642cb847b74a8e15ece0cc034bf7258009a90
Conor Jonassen <conor.jonassen@gmail.com>
2023-12-13 03:16:45 -0500

    asdsdasdasd

--- script.js
+++ script.js
@@ -3,17 +3,37 @@ function toggleDarkMode() {
             const isDarkMode = body.classList.contains('dark-mode');
 
             if (isDarkMode) {
-                // Switch to light mode
                 body.classList.remove('dark-mode');
                 document.querySelector("body").style.background= (body).background
                 document.querySelector("body").style.color= "#333"
-                
+
             } else {
-                // Switch to dark mode
                 body.classList.add('dark-mode');
                 document.querySelector("body").style.color = "white";
                 document.querySelector("body").style.background = "black";
             }
         }
 
+function contactValidation() {
+    const cntfm = document.contactForm;
+    let errors = [];
+
+    if (cntfm.text.value.trim() === "") {
+        document.getElementById("nameError").innerHTML = "Name is required";
+            return false;
+        } else {
+            document.getElementById("nameError").innerHTML = "";
+        }
+    
+        const emailField = cntfm.email;
+
+    if (emailField.type === "email" && !emailField.value.match(/^\S+@\S+\.\S+$/)) {
+        errors.push("Invalid email address");
+    }
+    
+        const message = "Thank you for subscribing to GrandLine Fruits!";
+        alert(message);
+    
+        return false;
+}
         
